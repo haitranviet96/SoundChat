b62fccac799a7fdd8508c4303fb36a877904ee79
Huy Nguyen <nqhuy1811@gmail.com>
2018-12-22 21:09:53 +0700

    Update

--- client/src/pages/chatPage/index.js
+++ client/src/pages/chatPage/index.js
@@ -18,7 +18,7 @@ class ChatPage extends React.Component {
     this.setState({ currentRoom: room })
   }
   componentDidMount = () => {
-    const socket = io('http://localhost:5000', { query: { jwt: sessionStorage.getItem('soundchat-access-token') } });
+    const socket = io('http://3.0.208.25', { query: { jwt: sessionStorage.getItem('soundchat-access-token') } });
     if (socket) {
       socket.connect();
       socket.on('my response', (data) => {
